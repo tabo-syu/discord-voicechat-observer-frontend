40f2a482ca7bf76c3809ebac85a6b9db4d9dfa7b
bambshot <takeshima.nitkc@gmail.com>
2021-09-13 01:58:20 +0900

    refactoring swr

--- utils/swr.ts
+++ utils/swr.ts
@@ -7,123 +7,64 @@ import {
   SessionLogResponse,
 } from './types';
 
-const domain = 'http://localhost:3001';
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
-
-export const useGuilds = () => {
-  const { data, error } = useSWR(`${domain}/guilds`, fetcher);
+const SWR = <ResponseType>(path: string) => {
+  const { data, error } = useSWR(`http://localhost:3001${path}`, fetcher);
 
   return {
-    data: data as GuildResponse[],
+    data: data as ResponseType,
     isLoading: !error && !data,
     isError: error,
   };
 };
 
-export const useGuild = (guildId: string) => {
-  const { data, error } = useSWR(`${domain}/guilds/${guildId}`);
+export const useGuilds = () => SWR<GuildResponse[]>('/guilds');
 
-  return {
-    data: data as GuildResponse,
-    isLoading: !error && !data,
-    isError: error,
-  };
+export const useGuild = (guildId: string) => {
+  return SWR<GuildResponse>(`/guilds/${guildId}`);
 };
 
 export const useGuildParticipants = (guildId: string) => {
-  const { data, error } = useSWR(`${domain}/guilds/${guildId}/users`);
+  return SWR<UserResponse[]>(`/guilds/${guildId}/users`);
+};
 
-  return {
-    data: data as UserResponse[],
-    isLoading: !error && !data,
-    isError: error,
-  };
+export const useGuildParticipantSessions = (
+  guildId: string,
+  userId: string
+) => {
+  return SWR<SessionResponse[]>(`/guilds/${guildId}/users/${userId}/sessions`);
 };
 
 export const useVoiceChannels = (guildId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/guilds/${guildId}/voiceChannels`,
-    fetcher
-  );
-
-  return {
-    data: data as VoiceChannelResponse[],
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return SWR<VoiceChannelResponse[]>(`/guilds/${guildId}/voiceChannels`);
 };
 
 export const useVoiceChannel = (voiceChannelId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/voiceChannels/${voiceChannelId}`,
-    fetcher
-  );
-
-  return {
-    data: data as VoiceChannelResponse,
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return SWR<VoiceChannelResponse>(`/voiceChannels/${voiceChannelId}`);
 };
 
 export const useVoiceChannelSessions = (voiceChannelId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/voiceChannels/${voiceChannelId}/sessions`,
-    fetcher
-  );
-
-  return {
-    data: data as SessionResponse[],
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return SWR<SessionResponse[]>(`/voiceChannels/${voiceChannelId}/sessions`);
 };
 
 export const useVoiceChannelSessionLogs = (voiceChannelId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/voiceChannels/${voiceChannelId}/sessionLogs`,
-    fetcher
+  return SWR<SessionLogResponse[]>(
+    `/voiceChannels/${voiceChannelId}/sessionLogs`
   );
-
-  return {
-    data: data as SessionLogResponse[],
-    isLoading: !error && !data,
-    isError: error,
-  };
 };
 
 export const useSession = (sessionId: string) => {
-  const { data, error } = useSWR(`${domain}/sessions/${sessionId}`, fetcher);
-
-  return {
-    data: data as SessionResponse,
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return SWR<SessionResponse>(`/sessions/${sessionId}`);
 };
 
 export const useSessionUsers = (sessionId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/sessions/${sessionId}/users`,
-    fetcher
-  );
-
-  return {
-    data: data as UserResponse[],
-    isLoading: !error && !data,
-    isError: error,
-  };
+  return SWR<UserResponse[]>(`/sessions/${sessionId}/users`);
 };
 
 export const useSessionSessionLogs = (sessionId: string) => {
-  const { data, error } = useSWR(
-    `${domain}/sessions/${sessionId}/sessionLogs`,
-    fetcher
-  );
+  return SWR<SessionLogResponse[][]>(`/sessions/${sessionId}/sessionLogs`);
+};
 
-  return {
-    data: data as SessionLogResponse[][],
-    isLoading: !error && !data,
-    isError: error,
-  };
+export const useUser = (userId: string) => {
+  return SWR<UserResponse>(`/users/${userId}`);
 };
