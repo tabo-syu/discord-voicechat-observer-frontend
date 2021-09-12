76d0c2ad5ba8886e24f3e3af5bf28226fa08f16c
bambshot <takeshima.nitkc@gmail.com>
2021-09-12 19:37:01 +0900

    move type def

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,15 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { AppPropsWithLayout } from '../utils/types';
+import type { ReactElement, ReactNode } from 'react';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
 
 const MyApp = ({ Component, pageProps }: AppPropsWithLayout) => {
   const getLayout = Component.getLayout ?? ((page) => page);
--- utils/types.ts
+++ utils/types.ts
@@ -1,15 +1,3 @@
-import type { ReactElement, ReactNode } from 'react';
-import type { NextPage } from 'next';
-import type { AppProps } from 'next/app';
-
-export type NextPageWithLayout = NextPage & {
-  getLayout?: (page: ReactElement) => ReactNode;
-};
-
-export type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
-};
-
 export type GuildResponse = {
   id: string;
   name: string;
